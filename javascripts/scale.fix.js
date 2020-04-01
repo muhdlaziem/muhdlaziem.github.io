827680312278d84d188782e4a85595c988f6eccb
muhdlaziem <muhdlaziem@gmail.com>
2020-04-01 13:21:57 +0800

    modal, overlay, download

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -28,45 +28,106 @@ function gestureStart() {
 
 
 
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
+// $(document).ready(function () {
+//   $(document).on("scroll", onScroll);
   
-  //smoothscroll
-  $('a[href^="#"]').on('click', function (e) {
-      e.preventDefault();
-      $(document).off("scroll");
+//   //smoothscroll
+//   $('a[href^="#"]').on('click', function (e) {
+//       e.preventDefault();
+//       $(document).off("scroll");
       
-      $('a').each(function () {
-          $(this).removeClass('active');
-      })
-      $(this).addClass('active');
+//       $('a').each(function () {
+//           $(this).removeClass('active');
+//       })
+//       $(this).addClass('active');
     
-      var target = this.hash,
-          menu = target;
-      $target = $(target);
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top+2
-      }, 500, 'swing', function () {
-          window.location.hash = target;
-          $(document).on("scroll", onScroll);
-      });
-  });
-});
+//       var target = this.hash,
+//           menu = target;
+//       $target = $(target);
+//       $('html, body').stop().animate({
+//           'scrollTop': $target.offset().top+2
+//       }, 500, 'swing', function () {
+//           window.location.hash = target;
+//           $(document).on("scroll", onScroll);
+//       });
+//   });
+// });
 
-function onScroll(event){
-  var scrollPos = $(document).scrollTop();
-  $('.topnav a').each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.menu').removeClass("active");
-          currLink.addClass("active");
-      }
-      else{
-          currLink.removeClass("active");
-      }
-  });
+// function onScroll(event){
+//   var scrollPos = $(document).scrollTop();
+//   $('.topnav a').each(function () {
+//       var currLink = $(this);
+//       var refElement = $(currLink.attr("href"));
+//       if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+//           $('.menu').removeClass("active");
+//           currLink.addClass("active");
+//       }
+//       else{
+//           currLink.removeClass("active");
+//       }
+//   });
+// }
+
+
+
+let modal = [document.getElementById("myModal-1"),
+              document.getElementById("myModal-2"),
+              document.getElementById("myModal-3"),
+              document.getElementById("myModal-4"),
+              document.getElementById("myModal-5"),
+              document.getElementById("myModal-6")];
+
+let btn = [document.getElementById("btn-1"),
+              document.getElementById("btn-2"),
+              document.getElementById("btn-3"),
+              document.getElementById("btn-4"),
+              document.getElementById("btn-5"),
+              document.getElementById("btn-6")];
+
+for(let i = 0; i<btn.length; ++i){
+  // When the user clicks anywhere outside of the modal, close it
+
+  btn[i].onclick = function() {
+    modal[i].style.display = "block";
+  }
+  
 }
+
+window.onclick = function(event) {
+  for(let i = 0; i < modal.length; ++i){
+    if (event.target == modal[i]) {
+      modal[i].style.display = "none";
+    }
+  }
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+var close = document.getElementsByClassName("closeButton");
+
+
+// When the user clicks on <span> (x), close the modal
+for(let i = 0; i<span.length; ++i){
+  span[i].onclick = function() {
+    modal[i].style.display = "none";
+    // modal_3.style.display = "none";
+
+
+  }
+}
+
+for(let i = 0; i<close.length; ++i){
+  close[i].onclick = function() {
+    modal[i].style.display = "none";
+    // modal_3.style.display = "none";
+
+  }
+}
+
+
+
 
 // function myFunction() {
 //   var x = document.getElementById("myTopnav");
